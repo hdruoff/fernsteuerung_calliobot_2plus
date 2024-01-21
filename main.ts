f8f54e58ffe2f9471228fe3fcd60f6d2d47fde12
ha-dida <hdruoff@gmx.de>
2024-01-21 20:34:37 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,12 +7,26 @@ radio.onReceivedValue(function (name, value) {
 })
 let motor_rechts = 0
 let motor_links = 0
+let y_achse = 0
+let x_achse = 0
 let led_y_wert = 0
 let led_x_wert = 0
 let geschwindigkeit = 0
 let richtung = 0
 radio.setGroup(1)
 radio.setTransmitPower(7)
+basic.forever(function () {
+    led.unplot(led_x_wert, led_y_wert)
+    x_achse = Math.map(input.acceleration(Dimension.X), -1023, 1023, -50, 50)
+    richtung = x_achse
+    y_achse = Math.map(input.acceleration(Dimension.Y), 1023, -1023, 0, 100)
+    geschwindigkeit = y_achse
+    led_x_wert = Math.map(richtung, -50, 50, 0, 5)
+    led_y_wert = Math.map(geschwindigkeit, 0, 100, 5, 0)
+    led.plot(led_x_wert, led_y_wert)
+    radio.sendValue("Geschwindigkeit", geschwindigkeit)
+    radio.sendValue("Richtung", richtung)
+})
 basic.forever(function () {
     led.unplot(led_x_wert, led_y_wert)
     led_x_wert = Math.map(richtung, -50, 50, 0, 5)
