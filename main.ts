414145426b71ca670f29000357312dc4960be1ba
ha-dida <hdruoff@gmx.de>
2024-01-30 18:58:10 +0100

    Fernsteuerung_Sound über Geschwindigkeit gesteuert

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+function Sound () {
+    music.ringTone(geschwindigkeit)
+}
 radio.onReceivedValue(function (name, value) {
     if (name == "Richtung") {
         richtung = value
@@ -7,25 +10,14 @@ radio.onReceivedValue(function (name, value) {
 })
 let motor_rechts = 0
 let motor_links = 0
-let y_achse = 0
-let x_achse = 0
 let led_y_wert = 0
 let led_x_wert = 0
-let geschwindigkeit = 0
 let richtung = 0
+let geschwindigkeit = 0
 radio.setGroup(1)
 radio.setTransmitPower(7)
 basic.forever(function () {
-    led.unplot(led_x_wert, led_y_wert)
-    x_achse = Math.map(input.acceleration(Dimension.X), -1023, 1023, -50, 50)
-    richtung = x_achse
-    y_achse = Math.map(input.acceleration(Dimension.Y), 1023, -1023, 0, 100)
-    geschwindigkeit = y_achse
-    led_x_wert = Math.map(richtung, -50, 50, 0, 5)
-    led_y_wert = Math.map(geschwindigkeit, 0, 100, 5, 0)
-    led.plot(led_x_wert, led_y_wert)
-    radio.sendValue("Geschwindigkeit", geschwindigkeit)
-    radio.sendValue("Richtung", richtung)
+	
 })
 basic.forever(function () {
     led.unplot(led_x_wert, led_y_wert)
@@ -35,13 +27,16 @@ basic.forever(function () {
     motor_links = geschwindigkeit + richtung
     motor_rechts = geschwindigkeit - richtung
     if (motor_links > 100) {
-        calliBot2.motor(C2Motor.links, C2Dir.vorwaerts, 0)
+        motor_links = 0
     } else if (motor_rechts > 100) {
-        calliBot2.motor(C2Motor.rechts, C2Dir.vorwaerts, 100)
+        motor_rechts = 0
     } else if (motor_rechts < 0) {
-        calliBot2.motor(C2Motor.rechts, C2Dir.vorwaerts, 0)
+        motor_rechts = 100
+    } else if (motor_links < 0) {
+        motor_links = 100
     } else {
         calliBot2.motor(C2Motor.links, C2Dir.vorwaerts, motor_links)
         calliBot2.motor(C2Motor.rechts, C2Dir.vorwaerts, motor_rechts)
     }
+    Sound()
 })
