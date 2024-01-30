ee5efbec1571d5286319da945ae80a3c32fb220b
ha-dida <hdruoff@gmx.de>
2024-01-30 19:59:02 +0100

    Abstanssensor und Stossstange stop ergänzt

--- main.ts
+++ main.ts
@@ -1,5 +1,15 @@
+function Abstandssensor () {
+    if (calliBot2.entfernung(C2Einheit.cm) < 10 || (calliBot2.readBumperSensor(C2Sensor.links, C2State.an) || calliBot2.readBumperSensor(C2Sensor.rechts, C2State.an))) {
+        geschwindigkeit = 0
+        calliBot2.motorStop(C2Motor.beide, C2Stop.Bremsen)
+    } else {
+        geschwindigkeit = geschwindigkeit
+        calliBot2.motor(C2Motor.links, C2Dir.vorwaerts, motor_links)
+        calliBot2.motor(C2Motor.rechts, C2Dir.vorwaerts, motor_rechts)
+    }
+}
 function Sound () {
-    music.ringTone(geschwindigkeit)
+    music.ringTone(geschwindigkeit * 10)
 }
 radio.onReceivedValue(function (name, value) {
     if (name == "Richtung") {
@@ -8,11 +18,11 @@ radio.onReceivedValue(function (name, value) {
         geschwindigkeit = value
     }
 })
-let motor_rechts = 0
-let motor_links = 0
 let led_y_wert = 0
 let led_x_wert = 0
 let richtung = 0
+let motor_rechts = 0
+let motor_links = 0
 let geschwindigkeit = 0
 radio.setGroup(1)
 radio.setTransmitPower(7)
@@ -24,6 +34,7 @@ basic.forever(function () {
     led_x_wert = Math.map(richtung, -50, 50, 0, 5)
     led_y_wert = Math.map(geschwindigkeit, 0, 100, 5, 0)
     led.plot(led_x_wert, led_y_wert)
+    Sound()
     motor_links = geschwindigkeit + richtung
     motor_rechts = geschwindigkeit - richtung
     if (motor_links > 100) {
@@ -35,8 +46,6 @@ basic.forever(function () {
     } else if (motor_links < 0) {
         motor_links = 100
     } else {
-        calliBot2.motor(C2Motor.links, C2Dir.vorwaerts, motor_links)
-        calliBot2.motor(C2Motor.rechts, C2Dir.vorwaerts, motor_rechts)
+        Abstandssensor()
     }
-    Sound()
 })
